14d96edde11ffc2820835def573fdd8d6f9f50fa
aprusty28 <aprusty@buffalo.edu>
2024-10-16 16:41:22 -0400

    Updated code

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,27 +11,35 @@ const About = () => {
         <div className={styles.content}>
             <img className={styles.aboutImage} src={getImageUrl("about/profile.jpg")} alt="My profile pic"></img>
             <ul className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon"/>
+                    <div className={styles.aboutItemText}>
+                        <h3>Full Stack Developer</h3>
+                        <p>
+                        Seamlessly integrated frontend and backend technologies for cohesive user experiences. Designed and managed SQL and NoSQL databases for efficient data handling. Implemented secure authentication (JWT/OAuth) to protect user data. Developed responsive applications optimized for various devices.
+                        </p>
+                    </div>
+                </li>
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon"/>
                     <div className={styles.aboutItemText}>
                         <h3>Frontend Developer</h3>
-                        <p>I'm a passionate Frontend developer with experience in building intuitive user interfaces</p>
+                        <p>
+                        Created user-friendly interfaces with HTML, CSS, JavaScript, React, and Next.js. Utilized Redux and Context API for effective application state management. Improved load times through lazy loading and code splitting. Leveraged Tailwind CSS and Bootstrap for consistent UI design.
+                            </p>
                     </div>
                 </li>
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon"/>
                     <div className={styles.aboutItemText}>
                         <h3>Backend Developer</h3>
-                        <p>I have experience in developing high-performance and developer friendly APIs</p>
+                        <p>
+                    Designed and implemented RESTful APIs for efficient client-server communication. Built microservices for modular and scalable application architecture. Ensured API security with robust error handling and input validation. Conducted unit tests and documented APIs using Postman and Swagger. 
+
+                        </p>
                     </div>
                 </li>
-                <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon"/>
-                    <div className={styles.aboutItemText}>
-                        <h3>Full Stack Developer</h3>
-                        <p>With expertise in both frontend and backend development, I build comprehensive solutions that integrate user-friendly interfaces with robust server-side logic</p>
-                    </div>
-                </li>
+                
             </ul>
         </div>
     </section>
@@ -39,3 +47,11 @@ const About = () => {
 }
 
 export default About
+
+{/* 
+Full-Stack Development
+Seamlessly integrated frontend and backend technologies for cohesive user experiences. Designed and managed SQL and NoSQL databases for efficient data handling. Implemented secure authentication (JWT/OAuth) to protect user data. Developed responsive applications optimized for various devices. Deployed applications on AWS/GCP using CI/CD pipelines for continuous integration.
+Frontend Development
+Created user-friendly interfaces with HTML, CSS, JavaScript, React, and Next.js. Utilized Redux and Context API for effective application state management. Developed applications adhering to WCAG guidelines for inclusivity. Improved load times through lazy loading and code splitting. Leveraged Tailwind CSS and Bootstrap for consistent UI design.
+Backend API Development
+Designed and implemented RESTful APIs for efficient client-server communication. Managed large datasets and implemented business logic for data integrity. Built microservices for modular and scalable application architecture. Ensured API security with robust error handling and input validation. Conducted unit tests and documented APIs using Postman and Swagger. */}
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,12 @@ const Hero = () => {
     <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Anuradha</h1>
-        <p className={styles.description}>I'm a full stack developer with 5 years of experience in React, Javascript and Nodejs. To know more about me, let's connect at</p>
+        <p className={styles.description}>
+        {/* Experienced Computer Science graduate with 5 years of experience in full stack and frontend development. Proficient in building responsive web applications, dynamic websites, and scalable platforms using modern technologies like React, JavaScript, TypeScript, Node.js, and AWS. Demonstrates expertise in creating seamless user experiences and optimizing performance, with hands-on experience in databases, APIs, and cloud-based solutions. Strong background in Agile methodologies and UI/UX design with a focus on delivering efficient, scalable, and maintainable code. Looking for roles in full-stack or frontend development.     */}
+        {/* Computer Science graduate with 5 years of experience in full-stack and frontend development. Proficient in building responsive web applications and scalable platforms using React, JavaScript, TypeScript, Node.js, and AWS. Expertise in creating seamless user experiences, optimizing performance, and working with databases and APIs. Strong background in Agile methodologies and UI/UX design, focused on delivering efficient and maintainable code. Seeking roles in full-stack or frontend development. */}
+        Full-stack and frontend developer with 5 years of experience. Proficient in React, JavaScript, TypeScript, Node.js, and AWS. Focused on building responsive applications and optimizing user experiences. Seeking opportunities in full-stack or frontend development.
+        </p>
+        
         <a className={styles.contactBtn} href='mailto:aprusty@buffalo.edu'>Contact Me</a>
     </div>
     <img className={styles.heroImg} src={getImageUrl("about/file.jpg")} alt="My profile pic"></img>
